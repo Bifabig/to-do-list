898987572197c19056438bbe828cc01a8b82fc05
Bifabig <biftubig@gmail.com>
2023-05-06 12:01:59 +0300

    remove unwanted comments

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import { renderTodos } from './functions.js';
 
 document.querySelector('.text-input').addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
-    // addTodo(e.target.value);
     renderTodos();
     e.target.value = '';
   }
 });
-
-// document.querySelector('.clear-btn').addEventListener('click', () => {
-//   deleteAll();
-// });
 
 renderTodos();
